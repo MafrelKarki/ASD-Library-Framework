0c198399cda517b893f0e3f62c599a8bf76f3c85
Mafrel Karki <37508637+MafrelKarki@users.noreply.github.com>
2018-05-19 00:36:03 -0500

    Add files via upload

---
+++ src/edu/mum/asd/library/dao/DB.java
@@ -0,0 +1,15 @@
+package edu.mum.asd.library.dao;
+
+import java.sql.*;
+
+public class DB {
+public static Connection getCon(){
+	Connection con=null;
+	try{
+		Class.forName("com.mysql.jdbc.Driver");
+		con=DriverManager.getConnection("jdbc:mysql://localhost:3306/elibrary?useSSL=false","root","root");
+		
+	}catch(Exception e){System.out.println(e);}
+	return con;
+}
+}
